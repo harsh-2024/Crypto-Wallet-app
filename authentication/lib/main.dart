389a59f2384826cb2e30d1a91a57545b03891837
Harsh <tripathiharsh16@gmail.com>
2022-04-16 11:44:36 +0530

    google sign in functionalty implemented using Firebase

---
+++ authentication/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:authentication/signIn.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ authentication/lib/signIn.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  Future<UserCredential> signIn() async {
+    final GoogleSignInAccount? googleSignInAccount =
+        await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleSignInAuthentication =
+        await googleSignInAccount?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication?.idToken,
+        accessToken: googleSignInAuthentication?.accessToken);
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase 🔥🔥'),
+        backgroundColor: Colors.black38,
+      ),
+      body: Container(
+        child: Center(
+            child: ElevatedButton(
+                onPressed: signIn, child: const Text('Sign In'))),
+      ),
+    );
+  }
+}
