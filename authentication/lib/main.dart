d82ffa9a0b64c04e7aa0db499eccfea468937cb3
Harsh <tripathiharsh16@gmail.com>
2022-04-16 22:28:35 +0530

    crud basic operations implementd

--- authentication/lib/main.dart
+++ authentication/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- authentication/lib/signIn.dart
+++ authentication/lib/signIn.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -31,11 +32,43 @@ class _WelcomePageState extends State<WelcomePage> {
     print('user signed out');
   }
 
+  DocumentReference documentReference =
+      FirebaseFirestore.instance.doc("myData/dummyData");
+  void _add() {
+    Map<String, String> data = <String, String>{
+      "name": "Harsh Tripathi",
+      "company": "FRE",
+      "projects": "2",
+      "add": "Jhansi"
+    };
+    documentReference.set(data).whenComplete(() => print('document added'));
+  }
+
+  void fetch() {
+    documentReference.get().whenComplete(() => print('document fetched!'));
+  }
+
+  void update() {
+    Map<String, String> updatedData = <String, String>{
+      "name": "Ansh Tripathi",
+      "company": "JPMC",
+      "projects": "4",
+      "add": "Jhansi"
+    };
+    documentReference
+        .update(updatedData)
+        .whenComplete(() => print("document updated"));
+  }
+
+  void _delete() {
+    documentReference.delete().whenComplete(() => print('document deleted'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Firebase 🔥🔥'),
+        title: Text('Firebase FireStore🔥🔥'),
         backgroundColor: Colors.black38,
       ),
       body: Container(
@@ -43,18 +76,25 @@ class _WelcomePageState extends State<WelcomePage> {
           children: [
             Center(
                 child: ElevatedButton(
-                    onPressed: signIn,
-                    child: const Text('SignIn with Google'))),
+                    onPressed: signIn, child: const Text('SignIn'))),
             Center(
                 child: ElevatedButton(
                     onPressed: signOut, child: const Text('Sign out'))),
             Center(
-                child: ElevatedButton(
-                    onPressed: null, child: Text('SignIn with Facebook'))),
+                child:
+                    ElevatedButton(onPressed: _add, child: Text('Add Data'))),
             // ignore: prefer_const_constructors
             Center(
                 child: ElevatedButton(
-                    onPressed: null, child: Text('SignIn with Github')))
+                    onPressed: fetch, child: Text('Fetch Data'))),
+            Center(
+                child: ElevatedButton(
+                    onPressed: update, child: Text('Update Data'))),
+            Center(
+                child: ElevatedButton(
+              onPressed: _delete,
+              child: Text('Delete Data'),
+            ))
           ],
         ),
       ),
