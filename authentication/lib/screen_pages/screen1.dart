f4329fedf9ef9362eb24e8e2e3e66864f2ebc7a2
Harsh <tripathiharsh16@gmail.com>
2022-04-28 17:52:19 +0530

    UI for BTC exchanges done

--- authentication/lib/screen_pages/screen1.dart
+++ authentication/lib/screen_pages/screen1.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Screen1 extends StatefulWidget {
   const Screen1({Key? key}) : super(key: key);
@@ -15,66 +18,107 @@ class _Screen1State extends State<Screen1> {
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Card(
-            margin: EdgeInsets.only(left: 20, top: 20),
+            margin: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 20),
             color: Colors.white10,
             child: SizedBox(
-              height: 200,
-              width: 300,
+              height: 400,
+              width: 500,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Amount ',
-                    style: TextStyle(color: Colors.white, fontSize: 40),
+                    ' Your Wallet ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w300),
                   ),
                   Text(
-                    'Invested ',
+                    ' ₹  --- ',
                     style: TextStyle(color: Colors.white, fontSize: 40),
                   ),
-                  Text(
-                    '   --- ',
-                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  SizedBox(
+                    height: 20,
                   ),
+                  InkWell(
+                    onTap: () => null,
+                    child: Container(
+                      child: Center(child: Text('Add Money')),
+                      height: 40,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade500,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
                   Row(
                     children: <Widget>[
-                      Card(
-                        child: InkWell(
-                            onTap: () => print('BTC'),
-                            splashColor: Colors.black45,
-                            child: SizedBox(
-                              height: 30,
-                              width: 50,
-                              child: Center(child: Text('BTC')),
-                            )),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Card(
-                        child: InkWell(
-                            onTap: () => print('BTC'),
-                            splashColor: Colors.black45,
-                            child: SizedBox(
-                              height: 30,
-                              width: 50,
-                              child: Center(child: Text('ETH')),
-                            )),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Card(
-                        child: InkWell(
-                            onTap: () => print('BTC'),
-                            splashColor: Colors.black45,
-                            child: SizedBox(
-                              height: 30,
-                              width: 50,
-                              child: Center(child: Text('SOL')),
-                            )),
+                      Expanded(
+                        child: Card(
+                          child: InkWell(
+                              onTap: () => print('BTC'),
+                              splashColor: Colors.black45,
+                              child: SizedBox(
+                                  height: 150,
+                                  width: 150,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        'BTC',
+                                        style: TextStyle(
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text('Invested: '),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text('Current: '),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Row(children: <Widget>[
+                                        InkWell(
+                                          onTap: () => null,
+                                          child: Container(
+                                            child: Center(child: Text('Buy')),
+                                            height: 40,
+                                            width: 100,
+                                            decoration: BoxDecoration(
+                                              color: Colors.grey,
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(width: 10),
+                                        InkWell(
+                                          onTap: () => null,
+                                          child: Container(
+                                            child: Center(child: Text('Sell')),
+                                            height: 40,
+                                            width: 100,
+                                            decoration: BoxDecoration(
+                                              color: Colors.grey,
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                          ),
+                                        ),
+                                      ]),
+                                    ],
+                                  ))),
+                        ),
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
