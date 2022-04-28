1bf009ed82568cb890c0919579d2e0ee8d53517a
Harsh <tripathiharsh16@gmail.com>
2022-04-28 16:59:14 +0530

    started working on HomePage UI

--- authentication/lib/screen_pages/screen1.dart
+++ authentication/lib/screen_pages/screen1.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class Screen1 extends StatefulWidget {
@@ -13,13 +12,73 @@ class _Screen1State extends State<Screen1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Welcome',
-          style: TextStyle(fontSize: 10, color: Colors.white),
-        ),
-      ),
-    );
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Card(
+            margin: EdgeInsets.only(left: 20, top: 20),
+            color: Colors.white10,
+            child: SizedBox(
+              height: 200,
+              width: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Amount ',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  Text(
+                    'Invested ',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  Text(
+                    '   --- ',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Card(
+                        child: InkWell(
+                            onTap: () => print('BTC'),
+                            splashColor: Colors.black45,
+                            child: SizedBox(
+                              height: 30,
+                              width: 50,
+                              child: Center(child: Text('BTC')),
+                            )),
+                      ),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      Card(
+                        child: InkWell(
+                            onTap: () => print('BTC'),
+                            splashColor: Colors.black45,
+                            child: SizedBox(
+                              height: 30,
+                              width: 50,
+                              child: Center(child: Text('ETH')),
+                            )),
+                      ),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      Card(
+                        child: InkWell(
+                            onTap: () => print('BTC'),
+                            splashColor: Colors.black45,
+                            child: SizedBox(
+                              height: 30,
+                              width: 50,
+                              child: Center(child: Text('SOL')),
+                            )),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
